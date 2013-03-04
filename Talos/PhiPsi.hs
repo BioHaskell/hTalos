80c9df9b6b691bb7768ef69ce7a158a7de15c4b4
Michal J. Gajda <mjgajda@gmail.com>
2013-03-04 18:04:31 +0100

    First working parse of Talos pHIpSI table.

--- Talos/PhiPsi.hs
+++ Talos/PhiPsi.hs
@@ -1,5 +1,12 @@
 {-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
-module Talos.PhiPsi where
+module Talos.PhiPsi( parsePhiPsi
+                   , parsePhiPsiFile
+                   , PhiPsi(..)      ) where
+
+import Control.Monad.Instances()
+import Data.Typeable
+import Data.List(partition)
+import Data.Either(partitionEithers)
 
 import qualified Data.ByteString.Char8 as BS
 
@@ -12,10 +19,44 @@ data PhiPsi = PhiPsi { phi, psi :: Double
                      }
   deriving (Eq, Show) -- add read later...
 
-parsePhiPsi :: BS.ByteString -> BS.ByteString -> [PhiPsi] 
-parsePhiPsi fname input = []
+parsePhiPsi :: BS.ByteString -> BS.ByteString -> ( [PhiPsi]
+                                                 , [String] )
+parsePhiPsi fname input = (goodRecords, errors)
   where
-    lines = BS.lines input
+    -- error handling:
+    errors = reports "Expected five columns in PhiPsi record, but found: " recordsOfInvalidLength ++
+             map withErrorSource recordParseErrors
+    reports msg ls = map (\x -> withErrorSource $ msg ++ show x) ls 
+    withErrorSource msg = "Error parsing " ++ BS.unpack fname ++ ": " ++ msg
+
+    -- filter out comments
+    (_comments, maybeRecords) = partition ((=='#') . BS.head) $ BS.lines input
+    -- filter out empty lines
+    records = filter (/=[]) . map BS.words $ maybeRecords
+    -- filter out records of bad length
+    (recordsOfGoodLength, recordsOfInvalidLength) = partition ((==5) . length) records
+    -- split off parse errors
+    (recordParseErrors,   goodRecords           ) = partitionEithers . map parseRecord $ recordsOfGoodLength
+    parseRecord [phiStr, psiStr, resIdStr, resNameStr, ssStr] =
+        do aPhi     <- parseCol phiStr
+           aPsi     <- parseCol psiStr
+           aResId   <- parseCol resIdStr
+           aSS      <- parseSS  ssStr
+           return $ PhiPsi aPhi aPsi aResId resNameStr aSS
+    parseCol :: (Read a, Typeable a) => BS.ByteString -> Either String a
+    parseCol c = case reads $ BS.unpack c of
+                   ((result, []):_) -> return result
+                   l                -> let typeHolder = fst $ head l
+                                       in colError c typeHolder
+    parseSS :: BS.ByteString -> Either String SSCode
+    parseSS "random" = return Loop -- not sure?
+    parseSS "beta"   = return Strand
+    parseSS "alpha"  = return Helix
+    parseSS c        = colError c Helix
+    colError c t = fail ( "Cannot parse column " ++
+                          (show . BS.unpack) c   ++
+                          " as "                 ++
+                          (show . typeOf) t ) -- t is never evaluated - just provides the type.
 
 parsePhiPsiFile fname = parsePhiPsi (BS.pack fname) `fmap` BS.readFile fname
                         
